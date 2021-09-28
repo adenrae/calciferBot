6e9b7e5d4a30ea05d304b86d973ce08cc957366c
Aden Carver <99aden@gmail.com>
2021-09-28 18:52:28 -0400

    testing
    testing basic discord APIs

---
+++ calcBot.py
@@ -0,0 +1,31 @@
+#  v1.0.0
+#  prints randomly chosen calcifer quotes
+
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    
+    print (
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild members: \n - {members}')
+
+client.run(TOKEN)

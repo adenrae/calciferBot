7f0d9b7309a4a212d94a7b11fc8aab3ea105de79
Aden Carver <99aden@gmail.com>
2021-09-28 22:22:45 -0400

    more lines

--- calcLines.py
+++ calcLines.py
@@ -4,4 +4,9 @@ calcLines = [
     "Easy. If you break the spell that chains me to this place, I'll break your spell in a flash",
     "Demons don't make promises.",
     "But I'm an exploited demon.",
+    "I'm chained here by a contract with Howl, who works me to death.",
+    "I'm the one who keeps this castle moving.",
+    "If you figure out the secret contract, the spell will break. If you do that, I'll break your spell too.",
+    "Granny. Granny! Boy, I've got my doubts about her.",
+    "22:49"
 ]
